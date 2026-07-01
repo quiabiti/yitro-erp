e4e030ead7c8f4831906c10ba8c0bd7b795cfcf2
Venâncio Quiabiti <quiabitinelson@gmail.com>
2026-07-01 15:46:13 +0100

    fix: Corrigir carregamento de dados da listagem de alunos
    - Removido label do apps.py para evitar conflito com escola.secretaria
    - alunos.html agora carrega dados do base_escola.html
    - Removidas referências a turmas-data e escola-data
    - Adicionada função carregarDadosDoBase()
    - Corrigido erro 'Elemento turmas-data não encontrado'

--- escola/secretaria/apps.py
+++ escola/secretaria/apps.py
@@ -3,5 +3,4 @@ from django.apps import AppConfig
 class SecretariaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'escola.secretaria'
-    label = 'escola_secretaria'
     verbose_name = 'Secretaria'
